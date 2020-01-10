462142db2a6191dee5eb64718a5f8db7610bab8f
Joël Heymbeeck <joel.heymbeeck@externel.be>
2020-01-10 13:42:19 +0100

    Deadfish added

---
+++ Deadfish/DeadfishInterpreter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Deadfish
+{
+    //https://esolangs.org/wiki/Deadfish#C.23
+    public class DeadfishInterpreter
+    {
+        private Dictionary<char, Func<int, int>> Commands { get; }
+
+        private string Program { get; set; }
+
+        public DeadfishInterpreter(Action<string> outputAction)
+        {
+            Commands = new Dictionary<char, Func<int, int>>
+            {
+                {'i', i => ++i},
+                {'d', i => --i},
+                {'s', i => i * i},
+                {
+                    'o', i =>
+                    {
+                        outputAction(i.ToString());
+                        return i;
+                    }
+                }
+            };
+        }
+
+        public void Parse(string s)
+        {
+            Program = s;
+        }
+
+        public void Execute()
+        {
+            int i = 0;
+            foreach (char cmd in Program.Where(x => Commands.Keys.Contains(x)))
+            {
+                i = i == 256 || i < 0 
+                    ? 0
+                    : i;
+                if (Commands.TryGetValue(cmd, out var func))
+                    i = func(i);
+            }
+        }
+    }
+}
---
+++ Deadfish/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Deadfish
+{
+    class Program
+    {
+        private static string Program0 = "iissso"; // should output 0
+        private static string Program288 = "diissisdo"; // should output 288
+        private static string Program0_2 = "iissisdddddddddddddddddddddddddddddddddo"; // should output 0
+
+        static void Main(string[] args)
+        {
+            DeadfishInterpreter interpreter = new DeadfishInterpreter(Console.Write);
+            interpreter.Parse(Program0_2);
+            interpreter.Execute();
+        }
+    }
+}
